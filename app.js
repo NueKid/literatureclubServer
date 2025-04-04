1ed0365bef609c06ba45931b5deeb9e0f4fa04ea
Nuekid <ryen.masters@gmail.com>
2025-04-03 17:17:15 -0700

    Passport JWT

--- app.js
+++ app.js
@@ -1,23 +1,25 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const logger = require('morgan');
+const passport = require('passport');
+const authenticate = require('./authenticate');
+const config = require('./config');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
 const campsiteRouter = require('./routes/campsiteRouter');
 
+// Mongoose Connection
 const mongoose = require('mongoose');
 
-const url = 'mongodb://localhost:27017/literatureclub';
+const url = config.mongoUrl;
 const connect = mongoose.connect(url, {});
-
 connect.then(() => console.log('Connected correctly to server'),
   err => console.log(err)
 );
 
-var app = express();
+const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -26,12 +28,31 @@ app.set('view engine', 'pug');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
+// app.use(cookieParser());
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/campsites', campsiteRouter);
+
+// Passport Initialize Authentication
+app.use(passport.initialize());
+app.use(passport.session());
+
+function auth(req, res, next) {
+  console.log(req.user);
+
+  if (!req.user) {
+    const err = new Error('You are not Authorized!');
+    err.status = 401;
+    return next(err);
+  } else {
+    return next();
+  }
+}
+
+app.use(auth);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
